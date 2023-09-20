3df779c08a221c02ee2c9be5bd3b1f7981725f30
kathimansilla <mansillakatherina@gmail.com>
2023-09-20 12:54:26 +0200

    add scss to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,20 +13,23 @@ const Form = ({ Link, newTaskObj }) => {
   /* Agregar input textarea para descripción, select para seleccionar categoría: urgente, moderado, no urgente, input para indicar deadline de la tarea*/
   return (
     <>
-      <form onSubmit={handleSubmit} className="form">
-        <h2 className="form__title">Nueva tarea</h2>
-        <input
-          className="form__input"
-          type="text"
-          placeholder="Nombre de la tarea"
-          name="taskName"
-          id="taskName"
-          onInput={handleInput}
-        />
-        <input type="submit" value="Guardar" className="form__buttonSave" />
-      </form>
-      <Link to="/TasksList">Ir a la lista de tareas</Link>
-      <Link to="/">Ir al inicio</Link>
+      <section className='formSection'>
+        <form onSubmit={handleSubmit} className="form">
+          <h2 className="form__title">Nueva tarea</h2>
+          <input
+            className="form__input"
+            type="text"
+            placeholder="Nombre de la tarea"
+            name="taskName"
+            id="taskName"
+            value={newTaskObj.taskName}
+            onInput={handleInput}
+          />
+          <input type="submit" value="Guardar" className="form__buttonSave" />
+        </form>
+        <Link to="/TasksList" className='form__link'>Ir a la lista de tareas</Link>
+        <Link to="/" className='form__link'>Ir al inicio</Link>
+      </section>
     </>
   );
 };
